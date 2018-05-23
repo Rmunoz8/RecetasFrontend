2684ee06c6d7ded449518e0d94b2252bee105a0b
Rubén <rmunoz@iesfuentesanluis.org>
2018-05-23 22:08:54 +0200

    Creación de usuarios y validaciones

--- src/app/components/registrer/registrer.component.ts
+++ src/app/components/registrer/registrer.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from "../../interfaces/usuario.interfaces";
 import { UsuarioService } from "../../services/usuario.service";
 import { Router, ActivatedRoute, Params } from "@angular/router";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { NG_VALIDATORS, Validator, Validators, AbstractControl, ValidatorFn, FormGroup, FormControl } from "@angular/forms";
 import { SnotifyService } from "ng-snotify";
+import { LoginService } from "../../services/login.service";
 
 @Component({
   selector: 'app-registrer',
@@ -14,54 +15,45 @@ export class RegistrerComponent implements OnInit {
 
   regForm: FormGroup;
   usuario: Usuario = {
-    _id:"",
-    apellido:"",
-    email:"",
-    image:"",
-    nick:"",
-    nombre:"",
-    password:"",
-    rol:""
+    _id: "",
+    apellido: "",
+    email: "",
+    image: "",
+    nick: "",
+    nombre: "",
+    password: "",
+    rol: ""
   };
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
     private _usuarioService: UsuarioService,
-    private _snotifyService: SnotifyService
-  ) {
-
-    this.regForm = new FormGroup({
-      nombre: new FormControl("", Validators.required),
-      apellido: new FormControl("", Validators.required),
-      nick: new FormControl("", [Validators.required,
-                                this.nombreCorrecto]),
-      email: new FormControl("", Validators.required),
-      password: new FormControl("", Validators.required),
-      password2: new FormControl("", [Validators.required,
-                                      this.samePass])
-    });
-
-    console.log('Formulario de registro creado');
-
-  }
+    private _snotifyService: SnotifyService,
+    private _loginService: LoginService
+  ) { }
 
   ngOnInit() {
+    this.regForm = new FormGroup({
+      nombre: new FormControl("", [Validators.required,
+      Validators.pattern("^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$")]),
+      apellido: new FormControl(""),
+      nick: new FormControl("", [Validators.required,
+      Validators.pattern("^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$")]),
+      email: new FormControl("", [Validators.required,
+      Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,20}/)
+      ]),
+      password2: new FormControl("", Validators.required)
+    }, this.samePass);
+
+    console.log('Formulario de registro creado');
   }
 
-  registro(){
-    
-  console.log(`USUARIO -> ${this.usuario.nombre}
-  ${this.usuario.apellido}
-  ${this.usuario.nick}
-  ${this.usuario.email}
-  ${this.usuario.password}`);
-  
-    
-  }
+  nombreCorrecto(control: FormControl): { [s: string]: boolean } {
 
-  nombreCorrecto(control: FormControl):{[s:string]:boolean}{
-
-    if(control.value.length <= 2){
+    if (control.value.length <= 2) {
       return {
         nombreCorrecto: true
       }
@@ -69,13 +61,81 @@ export class RegistrerComponent implements OnInit {
     return null;
   }
 
-  samePass(pass2: FormControl, pass1: FormControl):{[s:string]:boolean}{
-    if (pass2.value === pass1.value) {
-      return {
-        nombreCorrecto: true
-      }
-    }
-    return null;
+  samePass = function (fg: FormGroup) {
+    return fg.get('password').value === fg.get('password2').value ? null : { 'mismatch': true };
   }
 
+  faltanDatos() {
+    this._snotifyService.error(`Comprueba que los datos sean correctos`, {
+      timeout: 2000,
+      showProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false
+    });
+  }
+
+  registro() {
+
+    this._usuarioService.neuvoUsuario(this.usuario).subscribe(datos => {
+
+      if (datos.message) {
+        this._snotifyService.error(datos.message, {
+          timeout: 2000,
+          showProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false
+        });
+      } else {
+        this._snotifyService.success(`Usuario creado`, {
+          timeout: 2000,
+          showProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false
+        });
+
+        let userLog = {
+          email: this.usuario.email,
+          password: this.usuario.password,
+          gettoken: true
+        }
+        this._loginService.accesoUsuario(userLog).subscribe((datos) => {
+
+          if (!datos.message) {
+            userLog = {
+              email: this.usuario.email,
+              password: this.usuario.password,
+              gettoken: false
+            }
+            let token = datos;
+            this._loginService.accesoUsuario(userLog).subscribe((res) => {
+
+              let userIndentificado = res.usuario;
+              // Guardar informacion en el local storage
+              localStorage.setItem('userIndentificado', JSON.stringify(userIndentificado));
+              localStorage.setItem('token', JSON.stringify(token));
+              this._snotifyService.success(`Bienvenido ${this.usuario.nick} `, {
+                timeout: 2000,
+                showProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false
+              });
+              this._loginService.getFollows().subscribe((res) => {
+                console.log(`RES -> ${res} `);
+
+              });
+              this._router.navigate(['listaRecetas']);
+            });
+          } else {
+            this._snotifyService.error(datos.message, {
+              timeout: 2000,
+              showProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: false
+            });
+          }
+        });
+
+      }
+    })
+  };
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -8,6 +8,7 @@ export class UsuarioService {
 
   URL: string = "http://localhost:3800/api";
   URLgetAllUsuarios:string = "/allUsers";
+  URLnuevoUser:string = "/user";
 
   constructor(private http:Http) {
     console.log("Servicio Usuarios listo");
@@ -16,6 +17,21 @@ export class UsuarioService {
    getAllUsuarios(){     
      let url = `${this.URL}${this.URLgetAllUsuarios}`;     
      return this.http.get(url).map(res => res.json());
+   }
+
+   neuvoUsuario(usuario:Usuario){
+
+    let body = JSON.stringify(usuario);
+    let url = `${this.URL}${this.URLnuevoUser}`;
+    let headers = new Headers({
+       'Content-Type': 'application/json'
+    }); 
+
+    return this.http.post(url, body, {headers}).map(res =>{
+      console.log(res.json());
+      return res.json();
+    });
+
    }
 
 }
